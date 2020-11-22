8e285075b1cea2ff7eb9e4db4ed5cbc18cf64f18
John McLear <john@mclear.co.uk>
2020-11-22 16:38:46 +0000

    autofixes from Etherpad checkPlugins.js

--- index.js
+++ index.js
@@ -1,70 +1,69 @@
-var eejs = require('ep_etherpad-lite/node/eejs/');
-var settings = require('ep_etherpad-lite/node/utils/Settings');
+const eejs = require('ep_etherpad-lite/node/eejs/');
+const settings = require('ep_etherpad-lite/node/utils/Settings');
 
-var _ = require('ep_etherpad-lite/node_modules/underscore');
+const _ = require('ep_etherpad-lite/node_modules/underscore');
 
-var inlineMenuItems;
-var inlineButtons = [];
+let inlineMenuItems;
+let inlineButtons = [];
 
 exports.loadSettings = function (hook_name, context) {
-    inlineMenuItems = context.settings.toolbar.inline;
+  inlineMenuItems = context.settings.toolbar.inline;
 };
 
-exports.clientVars = function(hook, context, callback)
-{
+exports.clientVars = function (hook, context, callback) {
   // tell the client which year we are in
   createInlineToolbar();
-  return callback({ "ep_inline_toolbar": settings.ep_inline_toolbar || {}, "inlineButtons": inlineButtons });
+  return callback({ep_inline_toolbar: settings.ep_inline_toolbar || {}, inlineButtons});
 };
 
 var createInlineToolbar = function () {
-  var toolbar = this.toolbar;
+  const toolbar = this.toolbar;
   if (toolbar) {
-    var availableButtons = toolbar.availableButtons;
+    const availableButtons = toolbar.availableButtons;
     inlineButtons = [];
-    inlineMenuItems.forEach(function (inlineBlock) {
+    inlineMenuItems.forEach((inlineBlock) => {
       if (_.isArray(inlineBlock)) {
-          var buttons = [];
-          inlineBlock.forEach(function (buttonName) {
-            var buttonType = null;
-            var buttonTitle = null;
-            var localizationId = null;
-              if (_.isObject(buttonName)) {
-                var objKey = Object.keys(buttonName)[0];
-                var keySettings = buttonName[objKey];
-                buttonType = keySettings.buttonType;
-                buttonTitle = keySettings.title;
-                localizationId = keySettings.localizationId;
-                buttonName = objKey;
-              }
+        const buttons = [];
+        inlineBlock.forEach((buttonName) => {
+          let buttonType = null;
+          let buttonTitle = null;
+          let localizationId = null;
+          if (_.isObject(buttonName)) {
+            const objKey = Object.keys(buttonName)[0];
+            const keySettings = buttonName[objKey];
+            buttonType = keySettings.buttonType;
+            buttonTitle = keySettings.title;
+            localizationId = keySettings.localizationId;
+            buttonName = objKey;
+          }
 
-              if (availableButtons[buttonName]) {
-                  var buttonItem = availableButtons[buttonName];
-                  if (availableButtons[buttonName].attributes) {
-                    buttonItem = availableButtons[buttonName].attributes;
-                  }
+          if (availableButtons[buttonName]) {
+            let buttonItem = availableButtons[buttonName];
+            if (availableButtons[buttonName].attributes) {
+              buttonItem = availableButtons[buttonName].attributes;
+            }
 
-                  if (localizationId) {
-                    buttonItem.localizationId = localizationId;
-                    buttonTitle = localizationId;
-                  }
-                  buttonItem = toolbar.button(buttonItem);
+            if (localizationId) {
+              buttonItem.localizationId = localizationId;
+              buttonTitle = localizationId;
+            }
+            buttonItem = toolbar.button(buttonItem);
 
-                  var buttonHtml = buttonItem.render();
+            let buttonHtml = buttonItem.render();
 
-                  if (buttonType === 'link') {
-                    buttonHtml = buttonHtml.replace('<button', '<span').replace('</button>', '</span>').replace('data-type="button"', 'data-type="link"');
-                  }
+            if (buttonType === 'link') {
+              buttonHtml = buttonHtml.replace('<button', '<span').replace('</button>', '</span>').replace('data-type="button"', 'data-type="link"');
+            }
 
-                  if (buttonTitle) {
-                    buttonHtml = buttonHtml.replace('</span>', buttonTitle +'</span>');
-                  }
+            if (buttonTitle) {
+              buttonHtml = buttonHtml.replace('</span>', `${buttonTitle}</span>`);
+            }
 
-                  buttons.push(buttonHtml);
-              }
-            });
+            buttons.push(buttonHtml);
+          }
+        });
 
-          inlineButtons.push(buttons);
+        inlineButtons.push(buttons);
       }
     });
   }
@@ -77,13 +76,13 @@ exports.padInitToolbar = function (hook_name, context) {
 
 
 exports.eejsBlock_body = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_inline_toolbar/templates/menuButtons.ejs");
+  args.content += eejs.require('ep_inline_toolbar/templates/menuButtons.ejs');
 
   return cb();
 };
 
 exports.eejsBlock_mySettings = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_inline_toolbar/templates/settings.ejs");
+  args.content += eejs.require('ep_inline_toolbar/templates/settings.ejs');
   return cb();
 };
 
@@ -94,6 +93,6 @@ exports.eejsBlock_scripts = function (hook_name, args, cb) {
 
 // not used
 exports.eejsBlock_styles = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_inline_toolbar/templates/styles.html", {}, module);
+  args.content += eejs.require('ep_inline_toolbar/templates/styles.html', {}, module);
   return cb();
 };
--- static/js/index.js
+++ static/js/index.js
@@ -8,82 +8,82 @@ var Security = require('ep_etherpad-lite/static/js/security');
 var globalKey = 0;
 
 iT = {
-  hide: function(){
-    var padOuter = $('iframe[name="ace_outer"]').contents().find("body");
-    var inlineToolbar = padOuter.find("#inline_toolbar");
+  hide() {
+    const padOuter = $('iframe[name="ace_outer"]').contents().find('body');
+    const inlineToolbar = padOuter.find('#inline_toolbar');
     $(inlineToolbar).hide();
   },
-  show: function(selStart, selEnd){
-    var XY = getXYOffsetOfRep(selStart, selEnd);
+  show(selStart, selEnd) {
+    const XY = getXYOffsetOfRep(selStart, selEnd);
     this.draw(XY);
-    var padOuter = $('iframe[name="ace_outer"]').contents().find("body");
-    var inlineToolbar = padOuter.find("#inline_toolbar");
+    const padOuter = $('iframe[name="ace_outer"]').contents().find('body');
+    const inlineToolbar = padOuter.find('#inline_toolbar');
     $(inlineToolbar).show();
   },
-  draw: function(XY){
+  draw(XY) {
     drawAt(XY);
-  }
-}
+  },
+};
 
-exports.aceSelectionChanged = function(hook, context){
-  var selStart = context.rep.selStart;
-  var selEnd = context.rep.selEnd;
-  if((selStart[0] !== selEnd[0]) || (selStart[1] !== selEnd[1])){
+exports.aceSelectionChanged = function (hook, context) {
+  const selStart = context.rep.selStart;
+  const selEnd = context.rep.selEnd;
+  if ((selStart[0] !== selEnd[0]) || (selStart[1] !== selEnd[1])) {
     iT.show(selStart, selEnd);
-  }else{
+  } else {
     iT.hide();
   }
-}
+};
 
-exports.aceEditorCSS = function(hook, context){
-}
+exports.aceEditorCSS = function (hook, context) {
+};
 
 // Given a rep we get the X and Y px offset
-function getXYOffsetOfRep(selStart, selEnd){
-  var viewPosition = "top";
-  if(clientVars.ep_inline_toolbar && clientVars.ep_inline_toolbar.position){
+function getXYOffsetOfRep(selStart, selEnd) {
+  let viewPosition = 'top';
+  if (clientVars.ep_inline_toolbar && clientVars.ep_inline_toolbar.position) {
     viewPosition = clientVars.ep_inline_toolbar.position;
   }
-  var padOuter = $('iframe[name="ace_outer"]').contents();
-  var padInner = padOuter.find('iframe[name="ace_inner"]');
-  var topCorrection = padInner.offset().top;
-  var x = selStart[1];
-  var y = selEnd[0];
-  y = y+1;
-  var inner = $('iframe[name="ace_outer"]').contents().find('iframe');
-  var innerWidth = inner.contents().find('#innerdocbody').width();
+  const padOuter = $('iframe[name="ace_outer"]').contents();
+  const padInner = padOuter.find('iframe[name="ace_inner"]');
+  const topCorrection = padInner.offset().top;
+  const x = selStart[1];
+  let y = selEnd[0];
+  y += 1;
+  const inner = $('iframe[name="ace_outer"]').contents().find('iframe');
+  const innerWidth = inner.contents().find('#innerdocbody').width();
 
   // it appears on apple devices this might not be set properly?
-  if($(inner)[0]){
-    var leftOffset = $(inner)[0].offsetLeft +3;
-  }else{
+  if ($(inner)[0]) {
+    var leftOffset = $(inner)[0].offsetLeft + 3;
+  } else {
     var leftOffset = 0;
   }
 
-  var stickUp = false;
-  var stickLeft = true;
+  let stickUp = false;
+  const stickLeft = true;
 
   // Get the target Line
-  var line = getSelectedLineElement();
-  var divEl = $(line.lineNode);
-  var div = divEl.clone();
+  let line = getSelectedLineElement();
+  const divEl = $(line.lineNode);
+  const div = divEl.clone();
   inner.contents().find('body').append(div);
   div.css({position: 'absolute'});
   div.css(divEl.offset());
-  var divWidth = div.width();
+  const divWidth = div.width();
 
   // Is the line visible yet?
-  if ( divEl.length !== 0 ) {
-    var top = divEl.offset().top + topCorrection; // A standard generic offset
+  if (divEl.length !== 0) {
+    let top = divEl.offset().top + topCorrection; // A standard generic offset
     // Get the HTML
-    var html = $(div).html();
-    var text = $(div).text();
+    const html = $(div).html();
+    let text = $(div).text();
     text = Security.escapeHTML(text).split('');
-    
-    var workerIndex;
+
+    let workerIndex;
     if (viewPosition === 'right') {
       if (selEnd[0] > selStart[0] && selEnd[1] === 0) {
-        line =  getLineAtIndex(selEnd[0] -1);
+        line = getLineAtIndex(selEnd[0] - 1);
         workerIndex = $(line.lineNode).text().length - 1;
       } else {
         workerIndex = selEnd[1];
@@ -93,61 +93,61 @@ function getXYOffsetOfRep(selStart, selEnd){
     }
     text.splice(workerIndex, 0, '</span>');
     text.splice(workerIndex, 0, '<span id="selectWorker">');
-    var heading = isHeading();
+    const heading = isHeading();
     text = text.join('');
-    
+
     if (heading) {
-      text = '<'+heading+'>' + text + '</'+heading+'>';
+      text = `<${heading}>${text}</${heading}>`;
     }
 
     $(div).html(text);
-    var worker = $(div).find('#selectWorker');
-    var workerPosition = worker.position();
+    const worker = $(div).find('#selectWorker');
+    const workerPosition = worker.position();
 
     // Get the width of the element (This is how far out X is in px);
-    var left = workerPosition.left || 0;
+    let left = workerPosition.left || 0;
 
     // Get the height of the element minus the inner line height
     // Add the innerdocbody offset
-    left = left + leftOffset;
+    left += leftOffset;
 
     // Add support for page view margins
-    var divMargin = $(divEl).css("margin-left");
-    var innerdocbodyMargin = $(divEl).parent().css("margin-left"); 
-    if(innerdocbodyMargin){
-      innerdocbodyMargin = innerdocbodyMargin.replace("px", "");
+    let divMargin = $(divEl).css('margin-left');
+    let innerdocbodyMargin = $(divEl).parent().css('margin-left');
+    if (innerdocbodyMargin) {
+      innerdocbodyMargin = innerdocbodyMargin.replace('px', '');
       innerdocbodyMargin = parseInt(innerdocbodyMargin);
-    }else{
+    } else {
       innerdocbodyMargin = 0;
     }
-    if(divMargin){
-      divMargin = divMargin.replace("px", "");
+    if (divMargin) {
+      divMargin = divMargin.replace('px', '');
       divMargin = parseInt(divMargin);
-      if((divMargin + innerdocbodyMargin) > 0){
-        left = left + divMargin;
+      if ((divMargin + innerdocbodyMargin) > 0) {
+        left += divMargin;
       }
     }
-    //adjust position 
+    // adjust position
     if (viewPosition === 'top') {
-      top = top - padOuter.find("#inline_toolbar").height();
-    
-      if(top <= 0 ){  // If the tooltip wont be visible to the user because it's too high up
+      top -= padOuter.find('#inline_toolbar').height();
+
+      if (top <= 0) { // If the tooltip wont be visible to the user because it's too high up
         stickUp = true;
-        top = top + padOuter.find("#inline_toolbar").height();
-        if(top < 0){ top = 0; } // handle case where caret is in 0,0
+        top += padOuter.find('#inline_toolbar').height();
+        if (top < 0) { top = 0; } // handle case where caret is in 0,0
       }
     } else if (viewPosition === 'bottom') {
-      top = top + divEl.height();
+      top += divEl.height();
 
-      if(top >= padOuter.height() ){  // If the tooltip wont be visible to the user because it's too high up
+      if (top >= padOuter.height()) { // If the tooltip wont be visible to the user because it's too high up
         stickUp = true;
-        top = top - (padOuter.find("#inline_toolbar").height() * 2);
+        top -= (padOuter.find('#inline_toolbar').height() * 2);
       }
     } else if (viewPosition === 'right') {
-      top = top +($(div).height()/2);
+      top += ($(div).height() / 2);
     } else if (viewPosition === 'left') {
-      left = left - padOuter.find("#inline_toolbar").width();
-      top  = top +($(div).height()/2);
+      left -= padOuter.find('#inline_toolbar').width();
+      top += ($(div).height() / 2);
     }
     // Remove the element
     $(div).remove();
@@ -156,82 +156,77 @@ function getXYOffsetOfRep(selStart, selEnd){
 }
 
 // Draws the toolbar onto the screen
-function drawAt(XY){
-  
-  var padOuter = $('iframe[name="ace_outer"]').contents().find("body");
-  var toolbar = padOuter.find("#inline_toolbar");
+function drawAt(XY) {
+  const padOuter = $('iframe[name="ace_outer"]').contents().find('body');
+  const toolbar = padOuter.find('#inline_toolbar');
 
   toolbar.show();
   toolbar.css({
-    "position": "absolute",
-    "left": XY[0],
-    "top": XY[1]
+    position: 'absolute',
+    left: XY[0],
+    top: XY[1],
   });
 }
 
 
-
-function html_substr( str, count ) {
-  if( browser.msie ) return ""; // IE can't handle processing any of the X position stuff so just return a blank string
+function html_substr(str, count) {
+  if (browser.msie) return ''; // IE can't handle processing any of the X position stuff so just return a blank string
   // Basically the recursion makes IE run out of memory and slows a pad right down, I guess a way to fix this would be to
-  // only wrap the target / last span or something or stop it destroying and recreating on each change..  
+  // only wrap the target / last span or something or stop it destroying and recreating on each change..
   // Also IE can often inherit the wrong font face IE bold but not apply that to the whole document ergo getting teh width wrong
-  var div = document.createElement('div');
+  const div = document.createElement('div');
   div.innerHTML = str;
 
-  walk( div, track );
+  walk(div, track);
 
-  function track( el ) {
-    if( count > 0 ) {
-      var len = el.data.length;
+  function track(el) {
+    if (count > 0) {
+      const len = el.data.length;
       count -= len;
-      if( count <= 0 ) {
-        el.data = el.substringData( 0, el.data.length + count );
+      if (count <= 0) {
+        el.data = el.substringData(0, el.data.length + count);
       }
     } else {
       el.data = '';
     }
   }
 
-  function walk( el, fn ) {
-    var node = el.firstChild;
-    if(!node) return;
+  function walk(el, fn) {
+    let node = el.firstChild;
+    if (!node) return;
     do {
-      if( node.nodeType === 3 ) {
+      if (node.nodeType === 3) {
         fn(node);
         //          Added this >>------------------------------------<<
-      } else if( node.nodeType === 1 && node.childNodes && node.childNodes[0] ) {
-        walk( node, fn );
+      } else if (node.nodeType === 1 && node.childNodes && node.childNodes[0]) {
+        walk(node, fn);
       }
-    } while( node = node.nextSibling );
+    } while (node = node.nextSibling);
   }
   return div.innerHTML;
 }
 
 function wrap(target, key) { // key can probably be removed here..
- var newtarget = $("<div></div>");
+  const newtarget = $('<div></div>');
   nodes = target.contents().clone(); // the clone is critical!
-  if(key === true){ // We can probably remove all of thise..
+  if (key === true) { // We can probably remove all of thise..
     var key = 0; // Key allows us to increemnt an index inside recursion
   }
-  nodes.each(function() {
+  nodes.each(function () {
     if (this.nodeType == 3) { // text
-      var newhtml = "";
-      var text = this.wholeText; // maybe "textContent" is better?
-      for (var i=0; i < text.length; i++) {
-        if (text[i] == ' '){
-          newhtml += "<span data-key="+globalKey+"> </span>";
-        }
-        else
-        { 
-          newhtml += "<span data-key="+globalKey+">" + text[i] + "</span>";
+      let newhtml = '';
+      const text = this.wholeText; // maybe "textContent" is better?
+      for (let i = 0; i < text.length; i++) {
+        if (text[i] == ' ') {
+          newhtml += `<span data-key=${globalKey}> </span>`;
+        } else {
+          newhtml += `<span data-key=${globalKey}>${text[i]}</span>`;
         }
         key++;
         globalKey++;
       }
       newtarget.append($(newhtml));
-    }
-    else { // recursion FTW!
+    } else { // recursion FTW!
       $(this).html(wrap($(this), key)); // This really hurts doing any sort of count..
       newtarget.append($(this));
     }
@@ -241,22 +236,22 @@ function wrap(target, key) { // key can probably be removed here..
 
 exports.postAceInit = function (hook_name, context) {
   iT.hide();
-  var ace = context.ace;
-  var pad = context.pad;
+  const ace = context.ace;
+  const pad = context.pad;
 
-  var padOuter = $('iframe[name="ace_outer"]').contents().find("body");
-  padOuter.on('click', function () {
+  const padOuter = $('iframe[name="ace_outer"]').contents().find('body');
+  padOuter.on('click', () => {
     iT.hide();
   });
-  $("#inline_toolbar [data-key]").each(function () {
-    $(this).unbind("click");
-    var command = $(this).data('key');
-    
+  $('#inline_toolbar [data-key]').each(function () {
+    $(this).unbind('click');
+    const command = $(this).data('key');
+
     if ($(this).data('type') === 'link') {
       $(this).addClass('link');
-      var spanItem = $(this).find('span.buttonicon')[0];
-      var translationId = $(spanItem).data('l10n-id');
-      
+      const spanItem = $(this).find('span.buttonicon')[0];
+      const translationId = $(spanItem).data('l10n-id');
+
       $(spanItem).html(html10n.get(translationId));
     }
     $(this).on('click', function () {
@@ -265,78 +260,76 @@ exports.postAceInit = function (hook_name, context) {
     });
   });
 
-  $("#inline_toolbar").detach().appendTo(padOuter[0]);
-  
-  
-}
+  $('#inline_toolbar').detach().appendTo(padOuter[0]);
+};
 
-var lastLineSelectedIsEmpty = function(rep, lastLineSelected) {
-  var line = rep.lines.atIndex(lastLineSelected);
+var lastLineSelectedIsEmpty = function (rep, lastLineSelected) {
+  const line = rep.lines.atIndex(lastLineSelected);
   // when we've a line with line attribute, the first char line position
   // in a line is 1 because of the *, otherwise is 0
-  var firstCharLinePosition = (line.lineMarker === 1) ? 1 : 0;
-  var lastColumnSelected = rep.selEnd[1];
+  const firstCharLinePosition = (line.lineMarker === 1) ? 1 : 0;
+  const lastColumnSelected = rep.selEnd[1];
 
   return lastColumnSelected === firstCharLinePosition;
-}
+};
 
-var getLastLine = function(rep) {
-  var firstLine = rep.selStart[0];
-  var lastLineSelected = rep.selEnd[0];
+var getLastLine = function (rep) {
+  const firstLine = rep.selStart[0];
+  let lastLineSelected = rep.selEnd[0];
 
-  if (lastLineSelected > firstLine){
+  if (lastLineSelected > firstLine) {
     // Ignore last line if the selected text of it it is empty
-    if(lastLineSelectedIsEmpty(rep, lastLineSelected)){
+    if (lastLineSelectedIsEmpty(rep, lastLineSelected)) {
       lastLineSelected--;
     }
   }
   return lastLineSelected;
-}
+};
 
 var getLineAtIndex = function (index) {
   return this.rep.lines.atIndex(index);
-}
+};
 
 var isHeading = function (index) {
   var index;
   if (clientVars.ep_inline_toolbar.position === 'top') {
     index = this.rep.selStart[0];
   } else {
-    index =  getLastLine(this.rep);
+    index = getLastLine(this.rep);
   }
-  var attribs = this.documentAttributeManager.getAttributesOnLine(index);
-  for (var i=0; i<attribs.length; i++) {
+  const attribs = this.documentAttributeManager.getAttributesOnLine(index);
+  for (let i = 0; i < attribs.length; i++) {
     if (attribs[i][0] === 'heading') {
-      var value = attribs[i][1];
+      const value = attribs[i][1];
       i = attribs.length;
       return value;
     }
   }
   return false;
-}
+};
 
 var getSelectedLineElement = function () {
-  var index;
+  let index;
   if (clientVars.ep_inline_toolbar.position === 'top') {
     index = this.rep.selStart[0];
   } else {
-    index =  getLastLine(this.rep);
+    index = getLastLine(this.rep);
   }
 
-  var selectedLine = this.rep.lines.atIndex(index);
+  const selectedLine = this.rep.lines.atIndex(index);
   return selectedLine;
 };
 
-exports.aceInitialized = function(hook, context){
+exports.aceInitialized = function (hook, context) {
   getSelectedLineElement = _(getSelectedLineElement).bind(context);
   getLineAtIndex = _(getLineAtIndex).bind(context);
   isHeading = _(isHeading).bind(context);
-}
+};
 
 exports.postToolbarInit = function (hook, context) {
   if (clientVars.inlineButtons && clientVars.inlineButtons.length) {
-    $.each(clientVars.inlineButtons, function (key, item) {
+    $.each(clientVars.inlineButtons, (key, item) => {
       $('#inline_toolbar_menu_items').append(item);
     });
   }
-}
+};
